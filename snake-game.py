cace173ebd04fa25272685b80ce6520f4a3b9eef
Suvarnesh K M <51795492+SuvarneshKM@users.noreply.github.com>
2021-02-21 21:50:15 +0530

    Update snake-game.py

--- snake-game.py
+++ snake-game.py
@@ -111,13 +111,11 @@ while True:
         food_pos = [random.randrange(1, (frame_size_x//10)) * 10, random.randrange(1, (frame_size_y//10)) * 10]
     food_spawn = True
 
-    # GFX
     game_window.fill(black)
     for pos in snake_body:
 
         pygame.draw.rect(game_window, green, pygame.Rect(pos[0], pos[1], 10, 10))
 
-    # Snake food
     pygame.draw.rect(game_window, white, pygame.Rect(food_pos[0], food_pos[1], 10, 10))
 
     if snake_pos[0] < 0 or snake_pos[0] > frame_size_x-10:
