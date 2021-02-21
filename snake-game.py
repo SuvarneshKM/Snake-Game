de3f512e1e77d395d9313294ec5da192337fba2f
Suvarnesh K M <51795492+SuvarneshKM@users.noreply.github.com>
2021-02-21 21:50:29 +0530

    Update snake-game.py

--- snake-game.py
+++ snake-game.py
@@ -122,13 +122,11 @@ while True:
         game_over()
     if snake_pos[1] < 0 or snake_pos[1] > frame_size_y-10:
         game_over()
-    # Touching the snake body
     for block in snake_body[1:]:
         if snake_pos[0] == block[0] and snake_pos[1] == block[1]:
             game_over()
 
     show_score(1, white, 'consolas', 20)
-    # Refresh game screen
     pygame.display.update()
     # Refresh rate
     fps_controller.tick(difficulty)
